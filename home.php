90ad7d8842220aa344547cd86bdcc336df5ed085
kimberian <66874004+kimberian@users.noreply.github.com>
2020-07-25 16:26:57 +0800

    Added new files

---
+++ home.php
@@ -0,0 +1,210 @@
+<?php
+  session_start();
+  include("classes/connect.php");
+  include("classes/log-in.php");
+  $first_name = "";
+  $last_name = "";
+  $email = "";
+  $password = "";
+  $repassword = "";
+
+  
+
+
+
+  if (isset($_POST['login'])) {                                // if login
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+      // echo "<pre>";
+      // print_r($_POST);
+      // echo "</pre>";
+      $login = new login();
+      $result = $login->evaluate($_POST);
+      if ($result == ""){
+        header("Location:page.php");
+        die;
+      }else{
+        echo "error occured";
+        echo $result;
+      }
+
+    }
+    
+  }else if (isset($_POST['signup'])) {                                // if signup
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+      // echo "<pre>";
+      // print_r($_POST);
+      // echo "</pre>";
+      $signup = new signup();
+      $result = $signup->evaluate($_POST);
+      if ($result == "") {
+        header("Location:page.php");
+        die;
+      }else{
+        echo "error occured<br>";
+        echo $result;
+      }
+      
+    }
+  }
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Home | Shop-Cut </title>
+    <link rel="stylesheet" href="home-style.css?v=<?php echo time(); ?>">
+    <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+  </head>
+  <body>
+
+
+    <!-- Home                             -->
+    <section id=home>
+      <div class="menu-tab">
+        <h2><a href="#" style="text-decoration:none;">SHOP-cut</a></h2>
+        <span><ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#Product">Product</a>
+            <div class="product-submenu">
+              <ul>
+                <li><a href="#" >Food</a></li>
+                <li><a href="#">Accesories</a></li>
+                <li><a href="#">Gadgets</a></li>
+                <li><a href="#">Motor Parts</a></li>
+              </ul>
+            </div>
+          </li>
+          <li><a href="#Contact">Contact</a></li>
+          <li><a href="#Join_Us">Join Us</a></li>
+          <li><a href="#About_Us">About Us</a></li>
+        </ul></span>
+        <div class="log-in">
+          <form class=""  method="post">
+            <input type="email" name="email" value="<?php $email ?>" placeholder="Email">
+            <input type="password" name="password" value="<?php $password ?>" placeholder="Password">
+            <div class="log_in">
+              <input id="log_in" type="submit" name="login" value="Login">
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="row1">
+        <div class="content1">
+          <h3>We are Ease your shopping or selling online</h3>
+          <p>You can search what you need <br>
+            what you want anytime anywhere with <br>
+            our recommendations based on your location</p>
+        </div>
+      </div>
+    </section>
+
+    <section id="Product">
+      <div class="product-row">
+
+        <!-- slider -->
+        <div class="product-picture">
+          <input type="radio" name="image" id="i1" value="" checked>
+          <input type="radio" name="image" id="i2" value="">
+          <input type="radio" name="image" id="i3" value="">
+
+          <div class="product-picture-img" id="one">
+            <img src="Images/Home/image1.jpg" alt="">
+            <label for=""></label>
+          </div>
+          <div class="product-picture-img" id="two">
+            <img src="Images/Home/image2.jpg" alt="">
+          </div>
+          <div class="product-picture-img" id="three">
+            <img src="Images/Home/image1.jpg" alt="">
+          </div>
+          <div class="nav">
+            <label class="dots" id="dot1" for="i1"></label>
+            <label class="dots" id="dot2" for="i2"></label>
+            <label class="dots" id="dot3" for="i3"></label>
+          </div>
+
+        </div>
+        <div class="product-content">
+          <h3>Product</h3>
+          <p>You can search what you need <br>
+            what you want anytime anywhere with <br>
+            our recommendations based on your location</p>
+        </div>
+      </div>
+    </section>
+
+
+    <section id="Contact">
+      <div class="contact-row">
+        <div class="contact-content">
+          <h3>Contact Us</h3>
+          <form class="contact_form" method="post">
+            <input type="text" name="name" value="" placeholder="Your Name (e.g., Shaira Roc)"><br>
+            <input type="text" name="email" value="" placeholder="Your Email (email@example.com)"><br>
+            <textarea name="message" rows="8" cols="80" placeholder="Enter your question or message here"></textarea><br>
+            <!-- contact submit button -->
+            <input id="contact_submit" type="submit" name="contact_submit" value="Send Message">
+          </form>
+        </div>
+        <div class="contact-picture">
+          <img src="Images/Home/image1.jpg" alt="">
+        </div>
+      </div>
+    </section>
+
+
+
+    <section id="Join_Us">
+      <div class="join-row">
+        <div class="join-picture">
+          <img src="Images/Home/image1.jpg" alt="">
+
+        </div>
+        <div class="join-content">
+          <h3>Sign-Up</h3>
+          <form class="join_form" method="post">
+            <div class="names">
+              <input type="text" name="first_name" value="" placeholder="First Name">
+              <input type="text" name="last_name" value="" placeholder="Last Name">
+            </div>
+            <input type="text" name="email" value="" placeholder="Your Email (email@example.com)">
+            <input type="password" name="password" value="" placeholder="Password">
+            <input type="password" name="re-password" value="" placeholder="Confirm Password">
+            <!-- contact submit button -->
+            <input id="join_submit" type="submit" name="signup" value="sign-up">
+          </form>
+        </div>
+      </div>
+    </section>
+
+
+
+    <section id="About_Us">
+      <div class="about-row">
+        <div class="about-content">
+          <h3>About Us</h3>
+          <p>You can search what you need <br>
+            what you want anytime anywhere with <br>
+            our recommendations based on your location</p>
+        </div>
+        <div class="about-picture">
+          <img src="Images/Home/image1.jpg" alt="">
+        </div>
+      </div>
+    </section>
+
+
+    <script>
+    	var scroll = new SmoothScroll('a[href*="#"]',{
+        speed: 1000,
+        speedAsDuration:true
+      });
+    </script>
+
+
+  </body>
+</html>
